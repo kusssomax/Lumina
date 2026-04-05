1b4f4532ed6b1b9f41f88aa0d57f1065598802de
kusssomax <maxkusjo@gmail.com>
2026-04-05 14:18:55 +0300

    database setup / types and model schemas

---
+++ src/database/mongoose.ts
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+if (!MONGODB_URI) {
+    throw new Error('MONGODB_URI is not defined');
+}
+
+declare global {
+    var monggoseCache: {
+        conn: typeof mongoose | null;
+        promise: Promise<typeof mongoose> | null;
+    }
+}
+
+let cached = global.monggoseCache || ( global.monggoseCache = { conn: null, promise: null } );
+
+export const conectToMongoDB = async () => {
+    if (cached.conn) return cached.conn;
+
+    if (!cached.promise) {
+        cached.promise = mongoose.connect(MONGODB_URI, { bufferCommands: false, })
+    }
+
+    try {
+        cached.conn = await cached.promise;
+    } catch (error) {
+        cached.promise = null;
+        throw error;
+    }
+
+    console.info('Connected to MongoDB');
+    return cached.conn;
+}
